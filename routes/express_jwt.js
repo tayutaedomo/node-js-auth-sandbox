0401e762a32ff352ca66661ddcc04d3484f36aab
tayutaedomo <tayutaedomo@gmail.com>
2017-11-24 15:09:28 +0900

    Fix /express_jwt/authenticate

--- routes/express_jwt.js
+++ routes/express_jwt.js
@@ -7,6 +7,8 @@ var express = require('express');
 var router = express.Router();
 var jwt = require('jsonwebtoken');
 var config = require('../config/index');
+const passportConf = require('./passport');
+const helpers = require('../helpers/controller_helpers');
 
 var User = require('../models/user');
 
@@ -26,41 +28,75 @@ router.get('/setup', function(req, res) {
 });
 
 
-router.post('/authenticate', function(req, res) {
+router.post('/authenticate', async function(req, res) {
+  debug('authenticate', req.body);
+
   // find db by posted email
-  User.findOne({
-    email: req.body.email
-  }, function(err, user) {
-    if (err) throw err;
+  // User.findOne({
+  //   email: req.body.email
+  // }, function(err, user) {
+  //   if (err) throw err;
+  //
+  //   // validation
+  //   if (!user) {
+  //     res.json({
+  //       success: false,
+  //       message: 'Authentication failed. User not found.'
+  //     });
+  //     return;
+  //   }
+  //
+  //   if (user.password != req.body.password) {
+  //     res.json({
+  //       success: false,
+  //       message: 'Authentication failed. Wrong password.'
+  //     });
+  //     return;
+  //   }
+  //
+  //   // when valid -> create token
+  //   var token = jwt.sign(user, config.JWT_SECRET, {
+  //     expiresIn: '24h'
+  //   });
+  //
+  //   res.json({
+  //     success: true,
+  //     message: 'Authentication successfully finished.',
+  //     token: token
+  //   });
+  // });
+
+  try {
+    const user = await User.findOne({ email: req.body.email });
 
     // validation
-    if (!user) {
-      res.json({
-        success: false,
-        message: 'Authentication failed. User not found.'
-      });
-      return;
+    if (! user) {
+      throw new Error('Authentication failed. User not found.');
     }
 
-    if (user.password != req.body.password) {
-      res.json({
-        success: false,
-        message: 'Authentication failed. Wrong password.'
-      });
-      return;
+    const isMatch = await user.isValidPassword(req.body.password);
+
+    if (! isMatch) {
+      throw new Error('Authentication failed. Wrong password.');
     }
 
     // when valid -> create token
-    var token = jwt.sign(user, app.get('superSecret'), {
-      expiresIn: '24h'
-    });
+    var token = helpers.signToken(user);
 
     res.json({
       success: true,
       message: 'Authentication successfully finished.',
       token: token
     });
-  });
+
+  } catch (error) {
+    debug(error.stack);
+
+    res.json({
+      success: false,
+      message: error.message
+    });
+  }
 });
 
 
@@ -91,9 +127,9 @@ router.use(function(req, res, next) {
   });
 });
 
-
+//
 // secure api --------
-
+//
 
 router.get('/', function(req, res) {
   res.json({ message: 'Welcome to API routing'});
@@ -102,6 +138,7 @@ router.get('/', function(req, res) {
 router.get('/users', function(req, res) {
   User.find({}, function(err, users) {
     debug('users', users);
+
     if (err) throw err;
     res.json(users.length);
   });
